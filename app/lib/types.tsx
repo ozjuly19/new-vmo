489c0089ddcf41a630b862339c224eb2cf970728
William Largent <w.largent@athr.dev>
2024-05-22 23:59:56 -0600

    Implitmented a class for modal functionality from a central call.bk

--- app/lib/types.tsx
+++ app/lib/types.tsx
@@ -31,3 +31,15 @@ export interface PaginationProps {
     items: any[]; // The items to paginate
     tableRowRef: React.RefObject<HTMLTableRowElement>; // Reference to the table row element
 }
+
+// Represents the properties for the modal component
+import { ReactNode } from "react"; // Reqired type for declaring children prop
+export interface ModalProps {
+    title?: string; // The title of the modal
+    colorMode?: string; // The color mode of the modal (e.g., 'light' or 'dark')
+    showCloseButton?: boolean; // Whether to show the close button
+    showSaveButton?: boolean; // Whether to show the save button
+    onClose?: () => void; // Function to call when the modal is closed
+    onSave?: () => void; // Function to call when the save button is clicked
+    children: ReactNode; // The child elements to render inside the modal
+};
